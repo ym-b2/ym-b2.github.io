afd546d1f861ef1c045467b030158ed57ac483ab
yeemay <yeemay.basetwo@icloud.com>
2024-11-14 18:12:19 +0800

    added pwa prompt

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -9,7 +9,7 @@ if (!window._flutter) {
 _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e8570a","builds":[{"compileTarget":"dart2js","renderer":"canvaskit","mainJsPath":"main.dart.js"}]};
 
 
-const serviceWorkerVersion = "2932203608";
+const serviceWorkerVersion = "1240013098";
 
 let baseUri = document.baseURI;
 if (baseUri.endsWith('/')) {
